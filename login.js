cfb03d08575bd7c342167c43464a2f715923f214
Kartik Bhalerao <109031927+kartikbh6614@users.noreply.github.com>
2023-04-14 20:10:00 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,40 @@
+import "./IPhone8Plus1.css";
+
+const IPhone8Plus1 = () => {
+  return (
+    <div className="iphone-8-plus-1">
+      <i className="welcome">
+        <p className="welcome1">Welcome</p>
+      </i>
+      <input className="iphone-8-plus-1-child" type="password" autoFocus />
+      <div className="email-address">
+        <p className="welcome1">Email address</p>
+      </div>
+      <input
+        className="iphone-8-plus-1-item"
+        type="password"
+        required
+        autoFocus
+      />
+      <div className="password">Password</div>
+      <div className="iphone-8-plus-1-inner" />
+      <img
+        className="download-removebg-preview-1"
+        alt=""
+        src="/downloadremovebgpreview-1-1@2x.png"
+      />
+      <i className="gg">вαηgтιgα∂α</i>
+      <div className="log-in">Log in</div>
+      <div className="forgotten-password">
+        <span>Forgotten password</span>
+        <span className="span">?</span>
+      </div>
+      <img className="line-icon" alt="" src="/line-1.svg" />
+      <div className="new-here-sign">New here? Sign up.</div>
+      <div className="line-div" />
+      <div className="iphone-8-plus-1-child1" />
+    </div>
+  );
+};
+
+export default IPhone8Plus1;
